fa31aa0ca954cb39d31ede3c2bf27ed8b6b32279
ericlys <ericlysm@gmail.com>
2019-07-26 08:56:41 -0300

    Add endereço

---
+++ src/main/java/dominio/Endereco.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dominio;
+
+/**
+ *
+ * @author ericl
+ */
+public class Endereco {
+    private String rua;
+    private String bairrro;
+    private String cidade;
+    private String CEP;
+
+    public Endereco(String rua, String bairrro, String cidade, String CEP) {
+        this.rua = rua;
+        this.bairrro = bairrro;
+        this.cidade = cidade;
+        this.CEP = CEP;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getBairrro() {
+        return bairrro;
+    }
+
+    public void setBairrro(String bairrro) {
+        this.bairrro = bairrro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCEP() {
+        return CEP;
+    }
+
+    public void setCEP(String CEP) {
+        this.CEP = CEP;
+    }
+    
+    
+}
